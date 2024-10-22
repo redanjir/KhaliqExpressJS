576219c7a08fa0ac688a6eeea689631f9a9f6818
redanjir <glock68bedok@gmail.com>
2024-10-22 21:41:26 +0800

    tls true

--- src/config/dbConnection.mjs
+++ src/config/dbConnection.mjs
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () =>
 {
     try {
-        await  mongoose.connect(process.env.MONGODB_URI);
+        await  mongoose.connect(process.env.MONGODB_URI, {tls: true});
         console.log("Connected to Database");
     } catch (error) {
         console.log(`Error :${error}`);
